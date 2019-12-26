a330929634a48a97d12706a3510a3369c0e29735
NICHOLAS85 <15698885+NICHOLAS85@users.noreply.github.com>
2019-12-26 00:43:33 -0500

    *autoload: fix gh-r respecting ver-ice

--- zplugin-install.zsh
+++ zplugin-install.zsh
@@ -1091,7 +1091,7 @@ builtin source ${ZPLGM[BIN_DIR]}"/zplugin-side.zsh"
     local user="${reply[-2]}"
     local plugin="${reply[-1]}"
 
-    local url="https://github.com/$user/$plugin/releases/${ice[ver]}:-latest}"
+    local url="https://github.com/$user/$plugin/releases/${ice[ver]:-latest}"
 
     local -A matchstr=(
         "i386"    "(386|686)"
